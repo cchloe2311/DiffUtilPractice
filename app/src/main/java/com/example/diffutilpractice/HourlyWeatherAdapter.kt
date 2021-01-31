80ced6ff60483c1bf7df0b02daf1f404dd41664d
cchloe2311 <cchloe2311@daum.net>
2021-01-31 16:36:55 +0900

    added comments

--- app/src/main/java/com/example/diffutilpractice/HourlyWeatherAdapter.kt
+++ app/src/main/java/com/example/diffutilpractice/HourlyWeatherAdapter.kt
@@ -10,7 +10,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.diffutilpractice.model.HourlyWeatherModel
 import kotlinx.android.synthetic.main.item_hourly_weather.view.*
 
+/**
+ * ListAdapter를 상속받음
+ * - ListAdapter => public abstract class ListAdapter<T, VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH>
+ *                  final AsyncListDiffer<T> mDiffer;
+ *                  즉, Adapter 상속 및 내부적으로 AsyncListDiffer를 가져 ListAdapter 상속만으로 이를 사용할 수 있게 됨
+ *                  (AsyncListDiffer를 가져 submitList(), currentList를 그대로 사용할 수 있음)
+ */
 class HourlyWeatherAdapter : ListAdapter<HourlyWeatherModel, HourlyWeatherAdapter.HourlyWeatherViewHolder>(DiffCallback()) {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourlyWeatherViewHolder =
         HourlyWeatherViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_hourly_weather, parent, false))
 
