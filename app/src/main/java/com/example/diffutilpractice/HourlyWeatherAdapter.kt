cf30d26b2897b8ff205563e21044f91ef8abd5d4
cchloe2311 <cchloe2311@daum.net>
2021-01-31 16:10:53 +0900

    added explanation

--- app/src/main/java/com/example/diffutilpractice/HourlyWeatherAdapter.kt
+++ app/src/main/java/com/example/diffutilpractice/HourlyWeatherAdapter.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.item_hourly_weather.view.*
 
 class HourlyWeatherAdapter : RecyclerView.Adapter<HourlyWeatherAdapter.HourlyWeatherViewHolder>() {
 
+    // 구현한 DiffUtil.ItemCallback<T>로 객체 생성 및 내부 currentList를 사용
     private val asyncDiffer = AsyncListDiffer(this, HourlyWeatherDiffItemCallback())
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourlyWeatherViewHolder =
@@ -27,7 +28,7 @@ class HourlyWeatherAdapter : RecyclerView.Adapter<HourlyWeatherAdapter.HourlyWea
         /**
          * submitList 메소드의 역할
          * 1. 업데이트 해야 할 부분 확인
-         * 2. newList로 recyclerview list를 업데이트
+         * 2. newList로 recyclerview list를 업데이트 (Read-only라 해당 메소드로 업데이트 필요)
          */
     }
 
